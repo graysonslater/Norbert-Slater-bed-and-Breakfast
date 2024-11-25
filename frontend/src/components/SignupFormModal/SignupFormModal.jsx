e31e50243decc11864233b00deb3345c83417c30
graysonslater <graysonslater@gmail.com>
2024-11-24 21:29:03 -0500

    phase 4 code copied, NEEDS TO BE TESTEDgit add .!

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
-import './SignupForm.css';
+import './SignupFormPage.css';
 
 function SignupFormPage() {
   const dispatch = useDispatch();
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,48 +1,30 @@
-/***********************************************************************************************************************************************/
-//*                             IMPORTS
-/***********************************************************************************************************************************************/
-
-//imports the React library: create components, manage state, handle lifecycle methods
 import React from 'react';
-//used for rendering React components into the DOM
 import ReactDOM from 'react-dom/client';
-//imports the main application component
 import App from './App';
-//import styles for app
 import './index.css';
-//makes the Redux store available to all nested components in app
-import {Provider} from 'react-redux';
-//responsible for creating and configuring the Redux store: setting up reducers, middleware, enhancers 
-import configureStore from './store/store';
-// restoreCSRF restores or generate a new CSRF token, csrfFetch adds the CSRF token to the request headers
+import { Provider } from 'react-redux';
+import configureStore from './store/store.js';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-//gets session reducer and all aexported functions/thunks/objects
 import * as sessionActions from './store/session';
-
-/***********************************************************************************************************************************************/
-//*                             INITIALIZE STORE
-/***********************************************************************************************************************************************/
+import { Modal, ModalProvider } from './context/Modal.jsx';
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
-  //Making the following items globally accessable
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
 }
 
-/***********************************************************************************************************************************************/
-//*                             MAIN APPLICATION
-/***********************************************************************************************************************************************/
-
-//provider gives all fucntion components access to the store
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}> 
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
